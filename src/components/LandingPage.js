6fbba4f089d34a0b58629f129c53ddfd3dd12f44
Keyon-Jerome <keyonjerome@gmail.com>
2021-02-06 16:32:48 -0500

    Landing page commit.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,18 +7,39 @@ import IconButton from '@material-ui/core/IconButton';
 import AppAppBar from "./AppAppBar";
 // import MenuIcon from '@material-ui/core/MenuIcon';
 import backgroundSVG from '../assets/img/Home.svg';
-
+import { Row, Col, Container } from 'react-bootstrap';
 const styles = {
     landingPage: {
         backgroundImage:`url(${backgroundSVG})`
     }
+}
+const centerAll = {
+    verticalCenter: {
+        minHeight: '100%',  /* Fallback for browsers do NOT support vh unit */
+        minHeight: '100vh', /* These two lines are counted as one :-)       */
+      
+        display: 'flex',
+        alignItems: 
+        'center'
+      }
+
 }
 
 function LandingPage() {
     return (
         <div>
         <AppAppBar></AppAppBar>
-        <h1 >"HI"</h1>
+        <Container fluid style={styles.LandingPage}>
+  <Row>
+    <Col style={centerAll.verticalCenter} className="text-center" md={6}>
+        <div className="text-center">
+            <h1>Bonfire</h1>
+            <h2>Connect from Africa.</h2>
+        </div>
+    </Col>
+    <Col className="text-center" md={6}>1 of 1</Col>
+  </Row>
+</Container>
         </div>
     );
 }

25605a4730fefb3375d3e271a47f42933ce62300
Keyon-Jerome <keyonjerome@gmail.com>
2021-02-06 12:43:51 -0500

    Added material toolkit.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Signup from "./components/Signup";
 import Maps from "./components/Maps";
-
+import GridItem from "./ui-components/Grid/GridItem";
+import GridContainer from "./ui-components/Grid/GridContainer";
+import Button from "./ui-components/CustomButtons/Button";
 function App() {
   return (
     <Router>
@@ -22,6 +24,28 @@ function App() {
                   </Link>
                 </li>
                 <li className="nav-item">
+                <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              {/* <h1 className={classes.title}>Your Story Starts With Us.</h1> */}
+              <h4>
+                Every landing page needs a small description after the big bold
+                title, that{"'"}s why we added this text here. Add here all the
+                information that can make you or your product create the first
+                impression.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Watch video
+              </Button>
+            </GridItem>
+          </GridContainer>
                   <Link className="nav-link" to={"/signup"}>
                     Sign up
                   </Link>

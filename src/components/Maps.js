7fa3f74e88f4ff23567be9c6447a5e483feba1f7
abu-bello <77398705+abu-bello@users.noreply.github.com>
2021-02-07 02:27:20 -0500

    Changes

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -5,41 +5,11 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
+import Popup from "./Popup"
 
 const Maps = () => {
-  let location_ = "";
+  const [selected, setSelected] = useState({});
   const [data, setData] = useState([]);
-  const [mapRef, setMapRef] = useState(null);
-  const [selectedPlace, setSelectedPlace] = useState(null);
-  const [markerMap, setMarkerMap] = useState({});
-  const [infoOpen, setInfoOpen] = useState(false);
-
-  const loadHandler = (map) => {
-    // Store a reference to the google map instance in state
-    setMapRef(map);
-  };
-
-  // We have to create a mapping of our places to actual Marker objects
-  const markerLoadHandler = (marker, place) => {
-    return setMarkerMap((prevState) => {
-      return { ...prevState, [place.id]: marker };
-    });
-  };
-
-  const markerClickHandler = (event, place) => {
-    // Remember which place was clicked
-    setSelectedPlace(place);
-
-    // Required so clicking a 2nd marker works as expected
-    if (infoOpen) {
-      setInfoOpen(false);
-    }
-
-    setInfoOpen(true);
-
-    // if you want to center the selected Marker
-    //setCenter(place.pos)
-  };
 
   useEffect(() => doRequest(), []);
 
@@ -63,9 +33,10 @@ const Maps = () => {
       });
   };
 
-  // const onSelect = (item) => {
-  //   setSelected(item);
-  // };
+  const onSelect = (item) => {
+    setSelected(item);
+    console.log(selected)
+  };
 
   const mapStyles = {
     height: "100vh",
@@ -115,15 +86,6 @@ const Maps = () => {
     },
   ];
 
-  const locationgenerator = () => {
-    let google_location = {
-      lat: Math.random() * (+30.19334 - -30.68439978681 + 1) - 30.68439978681,
-      lng: Math.random() * (+40.19334 - -0.82333736817 + 1) - 0.82333736817,
-    };
-    location_ = google_location;
-    return google_location;
-  };
-
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
       <GoogleMap
@@ -131,47 +93,24 @@ const Maps = () => {
         zoom={3.3}
         center={defaultCenter}
       >
-        {data.length &&
-          data.map((person) => (
+        {data.length && data.map((person) => {
+          return (
             <Marker
-              key={person.Name}
-              position={locationgenerator()}
-              onLoad={(marker) => {
-                const customIcon = (opts) =>
-                  Object.assign(
-                    {
-                      path:
-                        "M7.8,1.3L7.8,1.3C6-0.4,3.1-0.4,1.3,1.3c-1.8,1.7-1.8,4.6-0.1,6.3c0,0,0,0,0.1,0.1 l3.2,3.2l3.2-3.2C9.6,6,9.6,3.2,7.8,1.3C7.9,1.4,7.9,1.4,7.8,1.3z M4.6,5.8c-0.7,0-1.3-0.6-1.3-1.4c0-0.7,0.6-1.3,1.4-1.3 c0.7,0,1.3,0.6,1.3,1.3 C5.9,5.3,5.3,5.9,4.6,5.8z",
-                      fillColor: "#f00",
-                      fillOpacity: 1,
-                      strokeColor: "#000",
-                      strokeWeight: 1,
-                      scale: 3.5,
-                    },
-                    opts
-                  );
-
-                marker.setIcon(
-                  customIcon({
-                    fillColor: "green",
-                    strokeColor: "white",
-                  })
-                );
-                return markerLoadHandler(marker, location_);
-              }}
-              onClick={(event) => markerClickHandler(event, location_)}
+              key={person.name}
+              position={person.Position}
+              onClick={() => onSelect(person)}
             />
-          ))}
-        {infoOpen && selectedPlace && (
-          <InfoWindow
-            position={location_}
-            clickable={true}
-            onCloseClick={() => setInfoOpen(false)}
-          >
-            <p>Hi</p>
-          </InfoWindow>
-        )}
-        ;
+          );
+        })}
+        {selected.Position &&(
+              <InfoWindow
+                position={selected.Position}
+                clickable={true}
+                onCloseClick={() => setSelected({})}
+              >
+                {<Popup Name={selected.Name} Languages={selected.Languages} Music={selected.Music} Arts={selected.Arts}/>}
+              </InfoWindow>
+            )};
       </GoogleMap>
     </LoadScript>
   );
